f14f004cfb5caf7ed7d514f979db72a6ae9ac09e
fatihabib <tjfatimah12@gmail.com>
2025-09-29 17:12:36 +0100

    Install whitenoise and configure static files

--- taxsite/settings.py
+++ taxsite/settings.py
@@ -17,6 +17,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -70,6 +71,7 @@ STATICFILES_DIRS = [
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 import os
 
